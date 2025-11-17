dabf269232895e3d27b2441c7a0dd22ff3a44104
MrRichar02 <richarmamadi03@gmail.com>
2025-11-16 20:53:35 -0500

    adding jal and semi jr

--- Test.asm
+++ Test.asm
@@ -1,9 +1,10 @@
 .text
 
 main: 
-add $s2, $s0, $s1
-sub $s3, $s0, $s1
-slt $s4, $s3, $s2
-beq $zero, $s2, main
-add $v0, $s3, $s4
+	nor $v0, $zero, $zero
+	jal second
+	nor $v0, $zero, $zero
+
+second:
+	j main
 
